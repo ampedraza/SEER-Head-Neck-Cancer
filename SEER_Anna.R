2e650938b3ab10c00bd5a91729893dea68b29bd7
Anna Cook <annacook@bu.edu>
2021-04-21 13:44:43 -0400

    EDA

--- SEER_Anna.R
+++ SEER_Anna.R
@@ -12,6 +12,7 @@ unique(cancer_data$`Year of Diagnosis`)
 unique(cancer_data$`Surgery Decision`)
 unique(cancer_data$Radiation)
 unique(cancer_data$Chemotherapy)
+unique(cancer_data$Insurance)
 
 ggplot(data = cancer_data) + geom_bar(aes(`Year of Diagnosis`))
 ggplot(data = cancer_data) + geom_bar(aes(Sex))
@@ -19,14 +20,30 @@ ggplot(data = cancer_data) + geom_bar(aes(Race))
 ggplot(data = cancer_data) + geom_bar(aes(Insurance))
 ggplot(data = cancer_data) + geom_bar(aes(`SEER Registry`))
 ggplot(data = cancer_data) + geom_bar(aes(`Median Household Income`))
-ggplot(data = cancer_data) + geom_bar(aes(`AJCC 7 Stage`))
-ggplot(data = cancer_data) + geom_bar(aes(`Cause of Death`))
-ggplot(data = cancer_data) + geom_bar(aes(`Surgery Performed?`))
-ggplot(data = cancer_data) + geom_bar(aes(`Surgery Decision`))
-ggplot(data = cancer_data) + geom_bar(aes(Radiation))
-ggplot(data = cancer_data) + geom_bar(aes(Chemotherapy))
 ggplot(data = cancer_data) + geom_bar(aes(Mets))
 ggplot(data = cancer_data) + geom_bar(aes(`Lymph Nodes`))
+ggplot(data = cancer_data) + geom_bar(aes(`Surgery Decision`))
+ggplot(data = cancer_data) + geom_bar(aes(Radiation))
+
+ggplot(data = cancer_data) + geom_bar(aes(`AJCC 7 Stage`, fill = Race)) +
+  facet_wrap(vars(Race))
+ggplot(data = cancer_data) + geom_bar(aes(`AJCC 7 Stage`, fill = Insurance)) +
+  facet_wrap(vars(Insurance))
+ggplot(data = cancer_data) + geom_bar(aes(`Cause of Death`, fill = Race)) +
+  facet_wrap(vars(Race))
+ggplot(data = cancer_data) + geom_bar(aes(`Cause of Death`, fill = Insurance)) +
+  facet_wrap(vars(Insurance))
+ggplot(data = cancer_data) + geom_bar(aes(`Surgery Performed?`, fill = Race)) +
+  facet_wrap(vars(Race))
+ggplot(data = cancer_data) + geom_bar(aes(`Surgery Performed?`, fill = Insurance)) +
+  facet_wrap(vars(Insurance))
+ggplot(data = cancer_data) + geom_bar(aes(Chemotherapy, fill = Race)) +
+  facet_wrap(vars(Race))
+ggplot(data = cancer_data) + geom_bar(aes(Chemotherapy, fill = Insurance)) +
+  facet_wrap(vars(Insurance))
+
+ggplot(data = cancer_data) + geom_bar(aes(Insurance, fill = Race)) +
+  facet_wrap(vars(Race))
 
 
 ggplot(data = cancer_data) +
@@ -37,7 +54,9 @@ ggplot(data = cancer_data) +
 
 
 
+### MODELING ###
 
-
-
+model1 <- glm(as.factor(`Surgery Performed?`) ~ Race + Insurance + `AJCC 7 Stage` + `Median Household Income`, data = cancer_data, family = binomial(link = "logit"))
+model1
+summary(model1)
 
